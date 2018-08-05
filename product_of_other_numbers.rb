1f2398f12ac898d10d51b6f5ac48a18cf32cc0b5
Danny Kirschner <rkirschner1377@gmail.com>
2018-08-05 15:43:44 -0400

    Updating product example

--- product_of_other_numbers.rb
+++ product_of_other_numbers.rb
@@ -28,8 +28,8 @@ def get_products_of_all_ints_except_at_index(int_array)
   # should return
   # 2 * 3
   # 1 * 3
-  # 3 * 1
-  # => [6, 3, 3]
+  # 2 * 1
+  # => [6, 3, 2]
   #
   # Task 1:
   # Give me all ints in array except current index int
